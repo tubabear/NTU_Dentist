04d8e2e2c8f17962e1b6f645d434dcf4d52870cc
tubabear <kensai1402@yahoo.com.tw>
2022-06-09 08:59:52 +0800

    modify

--- utils/dataset.py
+++ utils/dataset.py
@@ -2,7 +2,7 @@ import torch
 import os
 import pandas as pd
 import numpy as np
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import json
 
 class TeethDataset(torch.utils.data.Dataset):
@@ -29,13 +29,21 @@ class TeethDataset(torch.utils.data.Dataset):
         img = Image.open(img_path).convert("RGB")
         mask = Image.open(mask_path)
 
-        # change to numpyt format
+        # change image, mask into numpy format
         img = np.array(img, dtype=np.uint8)
         mask = np.array(mask, dtype=np.uint8)
-        
-        # instance are encoded as a different colors
+
+        if self.transforms is not None: # apply data augmentation
+            # transformed = self.transforms(image=img, mask=mask, bboxes=boxes)
+            transformed = self.transforms(image=img, mask=mask)
+            img = transformed["image"]
+            mask = transformed["mask"]
+            # boxes = np.array(transformed["bboxes"])
+        else: 
+            mask = mask.as_tensor(mask, dtype=torch.uint8)
+
         # split the color-encoded mask into a set of binary masks
-        masks = (mask == self.obj_ids.numpy()[:, None, None]).all(-1)
+        masks = (mask == self.obj_ids[:, None, None]).all(-1)
 
         # get bounding box coordinates for each mask
         boxes = []
@@ -48,87 +56,76 @@ class TeethDataset(torch.utils.data.Dataset):
                 ymin = np.min(pos[0])
                 ymax = np.max(pos[0])
                 boxes.append([xmin, ymin, xmax, ymax])
-            except ValueError: # else
+            except ValueError: # empty mask
                 empty_masks.append(i)
 
-
-        # apply data augmentation on both image and mask and bounding box
-        if self.transforms is not None:
-            transformed = self.transforms(image=img, mask=mask, bboxes=boxes)
-            img = transformed["image"]
-            mask = transformed["mask"]
-            boxes = np.array(transformed["bboxes"])
-
-        masks = (mask == self.obj_ids[:, None, None]).all(-1)
+        # don't keep empty mask
         masks = np.delete(masks.numpy(), empty_masks, axis=0)
 
-        image_id = torch.tensor([index])
+        boxes = np.array(boxes)
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
 
-        # there is only one class
-        labels = np.array(np.zeros(len(boxes), dtype=np.int64))
+        # only one class
+        labels = np.array(np.ones(len(boxes), dtype=np.int64))
 
         target = {}
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
         target["boxes"] = boxes
+        labels = torch.as_tensor(labels, dtype=torch.int64)
         target["labels"] = labels
+        image_id = torch.tensor([index])
         target["image_id"] = image_id
+        area = torch.as_tensor(area, dtype=torch.float32)
         target["area"] = area
         masks = torch.as_tensor(masks, dtype=torch.uint8)
         target["masks"] = masks
+        img = torch.as_tensor(img/255, dtype=torch.float32)
 
-        return img/255, target
-
+        return img, target
 """
-# demo tooth dataset
-
 file_root = "../data/"
-csv_file = "../file.csv"
+csv_file = "../file_.csv"
 teeth_rgb = "../teeth_rgb.json"
 
 import albumentations as A
 import albumentations.augmentations.transforms as T
 from albumentations.pytorch import ToTensorV2
 
-# data augmentations
 transforms = A.Compose([
-                        T.PadIfNeeded(640,640),
+                        A.LongestMaxSize(1024),
+                        T.PadIfNeeded(1024,1024, border_mode=0,),
                         A.Rotate(limit=180, border_mode=0, p=1),
                         ToTensorV2(),
                         ],
-                        bbox_params=A.BboxParams(
-                            format="pascal_voc",
-                            label_fields=[],
-                            )
             )
 
-# load data
 dataset = TeethDataset(file_root, csv_file, teeth_rgb, transforms=transforms)
-img, target_dict = dataset[12]
+for i in range(len(dataset)):
+    img, target_dict = dataset[i]
+    img = img.numpy()
+    img = np.moveaxis(img, 0, -1)
+    img = np.array(img*255, dtype=np.uint8)
 
-# change into numpy format
-img = img.numpy()
-img = np.moveaxis(img, 0, -1)
-img = np.array(img*255, dtype=np.uint8)
+    masks = target_dict["masks"]
+    for mask in masks:
+        mask = mask.numpy()
+        color = list(np.random.choice(range(256), size=3))
+        img[np.where(mask)] = color
 
-# mask each tooth on image by random color
-masks = target_dict["masks"]
-for mask in masks:
-    mask = mask.numpy()
-    color = list(np.random.choice(range(256), size=3))
-    img[np.where(mask)] = color
+    img = Image.fromarray(img)
 
-# draw bounding box of each tooth on image
-img = Image.fromarray(img)
-boxes = target_dict["boxes"]
-for box in boxes:
-    x1, y1, x2, y2 = box
-    x1 = int(x1.item())
-    y1 = int(y1.item())
-    x2 = int(x2.item())
-    y2 = int(y2.item())
-    color = tuple(np.random.choice(range(256), size=3))
-    draw = ImageDraw.Draw(img)
-    draw.line([(x1,y1),(x2,y1),(x2,y2),(x1,y2),(x1,y1)], width=3, fill=color)
-img.show()
+    boxes = target_dict["boxes"]
+    areas = target_dict["area"]
+    for box, area in zip(boxes, areas):
+        x1, y1, x2, y2 = box
+        x1 = int(x1.item())
+        y1 = int(y1.item())
+        x2 = int(x2.item())
+        y2 = int(y2.item())
+        color = tuple(np.random.choice(range(256), size=3))
+        draw = ImageDraw.Draw(img)
+        draw.line([(x1,y1),(x2,y1),(x2,y2),(x1,y2),(x1,y1)], width=3, fill=color)
+        draw.text((x1,y1), f"{int(area.item())}")
+
+    img.show()
 """
